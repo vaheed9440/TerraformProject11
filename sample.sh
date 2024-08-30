79f57263d54bef36c0503c5542fdbeb50a30c081
Abdul Vaheed <124774319+vaheed9440@users.noreply.github.com>
2024-08-30 20:51:46 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
-#
-
-sudo yum update -y
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd~
-~
-~
-
+#! /bin/bash
+yum install httpd git -y
+systemctl start httpd
+systemctl status httpd
+cd /var/www/html
+git clone https://github.com/RAHAMSHAIK007/netflixcodde.git
+mv netflixcodde/* .
