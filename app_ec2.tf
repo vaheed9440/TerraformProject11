6aff38a4eff376857187a65860ef2573547beb38
Abdul Vaheed <124774319+vaheed9440@users.noreply.github.com>
2024-08-30 19:33:21 +0530

    Update app_ec2.tf

--- app_ec2.tf
+++ app_ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "public_instance" {
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet.id
   associate_public_ip_address = true
-  key_name                    = "ABCD"
+  key_name                    = "devops"
 
   tags = {
     Name = "app_server"
